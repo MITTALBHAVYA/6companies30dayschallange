5927364a94553944b3ee43dc7416036909112907
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2024-01-04 22:36:49 +0530

    DAY 1

---
+++ find-consecutive-integers-from-a-data-stream.cpp
@@ -0,0 +1,31 @@
+class DataStream {
+public:
+    int value;
+    int k;
+    int count=0;
+    DataStream(int value, int k) {
+        this->value=value;
+        this->k=k;
+    }
+    
+    bool consec(int num) {
+        if(num==value){
+            count++;
+        }
+        else{
+            count=0;
+        }
+        if(count>=k){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * DataStream* obj = new DataStream(value, k);
+ * bool param_1 = obj->consec(num);
+ */
