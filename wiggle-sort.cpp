2ca8d2b1315b76b22ceeadab745a86187eb6189c
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2024-01-09 16:00:01 +0530

    DAY 5

---
+++ wiggle-sort.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int>temp(n);
+        int i;
+        int j=n-1;
+        if(n%2==0){
+            i=(n/2 - 1);
+        }
+        else{
+            i = n/2;
+        }
+
+        int k=0;
+
+        while((i>=0 || j>i)&& k < n){
+            if(k%2==0){
+                temp[k]=nums[i];
+                i--;
+            }
+            else{
+                temp[k]=nums[j];
+                j--;
+            }
+            k++;
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=temp[i];
+        }
+    }
+};
