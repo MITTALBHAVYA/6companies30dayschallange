69a87ada2ad11127cf309748fc28d12022730c20
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2024-01-09 19:32:02 +0530

    DAY 5

---
+++ shopping-offers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // Twisted Knapsack
+    unordered_map<string,int>mp;
+    int shoppingOffers(vector<int> &price, vector<vector<int>> &special, vector<int> &needs) {
+        string needString="";
+        int minPrice = 0;
+        for (int i = 0; i < needs.size(); ++i) {
+            minPrice += needs[i] * price[i];
+            needString+=char('0'+needs[i]);
+        }
+        needString+=char('0'+needs.size());
+        if(mp.find(needString) != mp.end()){
+            return mp[needString];
+        }
+        for (const auto &offer : special) {
+            bool validOffer = true;
+
+            for (int i = 0; i < needs.size(); ++i) {
+                if (offer[i] > needs[i]) {
+                    validOffer = false;
+                    break;
+                }
+            }
+
+            if (validOffer) {
+                for (int i = 0; i < needs.size(); ++i) {
+                    needs[i] -= offer[i]; // Altering the needs vector
+                }
+                minPrice = min(minPrice, offer.back() + shoppingOffers(price, special, needs));
+                for (int i = 0; i < needs.size(); ++i) {
+                    needs[i] += offer[i]; // Restoring the needs vector to its original state
+                }
+            }
+        }
+
+        return mp[needString]=minPrice;
+    }
+};
